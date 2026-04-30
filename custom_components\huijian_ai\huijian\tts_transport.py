96fbbb11fbda8bc50b5f61c1300af0f8f98cd558
fangwenyi <641235014@qq.com>
2026-04-30 10:58:05 +0800

    Add custom_components\huijian_ai\huijian\tts_transport.py

---
+++ custom_components\huijian_ai\huijian\tts_transport.py
@@ -0,0 +1,56 @@
+import logging
+import anyio
+
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.exceptions import ConfigEntryAuthFailed
+
+from . import Dict, EntryAuthFailedError, get_entry_data
+from .ws_transport import WsTransport
+
+_LOGGER = logging.getLogger(__name__)
+ATTR_ENDPOINT = "tts_endpoint"
+ATTR_TRANSPORT = "tts_transport"
+
+
+def get_entry_transport(hass: HomeAssistant, entry: ConfigEntry) -> "TtsTransport":
+    """Set up from a config entry."""
+    endpoint: str | None = entry.data.get(ATTR_ENDPOINT)
+    if not endpoint:
+        raise EntryAuthFailedError(hass, entry)
+    
+    this_data: dict = get_entry_data(hass, entry)
+    transport: TtsTransport | None = this_data.get(ATTR_TRANSPORT)
+    if transport and transport.endpoint == endpoint and transport.available:
+        return transport
+    
+    _LOGGER.info("Creating new TtsTransport for entry: %s %s", entry.entry_id, entry.title)
+    transport = TtsTransport(hass, entry, endpoint, ATTR_ENDPOINT, _LOGGER)
+    this_data[ATTR_TRANSPORT] = transport
+    return transport
+
+
+class TtsTransport(WsTransport):
+    _transport_type = "tts"
+
+    async def await_message(self, timeout: int = 60):
+        """Wait response message"""
+        try:
+            with anyio.fail_after(timeout):
+                async for data in self._recv_reader:
+                    if isinstance(data, bytes):
+                        yield data
+                    elif data.state == "stop":
+                        break
+                    else:
+                        self.logger.info("Received unknown message: %s", data)
+        except TimeoutError:
+            yield Dict(error="Response timeout")
+
+    async def async_remove_entry(self):
+        entry = self.entry
+        this_data: dict = get_entry_data(self.hass, entry)
+        transport: TtsTransport | None = this_data.pop(ATTR_TRANSPORT, None)
+        self.logger.info("Remove entry from TTS transport: title=%s id=%s", entry.title, entry.entry_id)
+        if transport:
+            await transport.stop("Remove entry")
